ac8085659060bc83311e9851eac1bfca9272af67
Andy Gao <agao@andrew.cmu.edu>
2020-04-29 14:55:17 -0400

    rebuilding site Wed Apr 29 14:55:17 EDT 2020

---
+++ static/js/fastsearch.js
@@ -0,0 +1,190 @@
+var fuse; // holds our search engine
+var searchVisible = false;
+var firstRun = true; // allow us to delay loading json data unless search activated
+var list = document.getElementById("searchResults"); // targets the <ul>
+var first = list.firstChild; // first child of search list
+var last = list.lastChild; // last child of search list
+var maininput = document.getElementById("searchInput"); // input box for search
+var resultsAvailable = false; // Did we get any search results?
+
+// ==========================================
+// The main keyboard event listener running the show
+//
+document.addEventListener("keydown", function (event) {
+  // CMD-/ to show / hide Search
+  if (event.metaKey && event.which === 18) {
+    // Load json search index if first time invoking search
+    // Means we don't load json unless searches are going to happen; keep user payload small unless needed
+    if (firstRun) {
+      loadSearch(); // loads our json data and builds fuse.js search index
+      firstRun = false; // let's never do this again
+    }
+
+    // Toggle visibility of search box
+    if (!searchVisible) {
+      document.getElementById("fastSearch").style.visibility = "visible"; // show search box
+      document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
+      searchVisible = true; // search visible
+    } else {
+      document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
+      document.activeElement.blur(); // remove focus from search box
+      searchVisible = false; // search not visible
+    }
+  }
+
+  // Allow ESC (27) to close search box
+  if (event.keyCode == 27) {
+    if (searchVisible) {
+      document.getElementById("fastSearch").style.visibility = "hidden";
+      document.activeElement.blur();
+      searchVisible = false;
+    }
+  }
+
+  // allow outside click
+  if (hideOnClickOutside(document.getElementById("fastSearch"))) {
+    if (searchVisible) {
+      document.getElementById("fastSearch").style.visibility = "hidden";
+      document.activeElement.blur();
+      searchVisible = false;
+    }
+  }
+  // DOWN (40) arrow
+  if (event.keyCode == 40) {
+    if (searchVisible && resultsAvailable) {
+    //   console.log("down");
+      event.preventDefault(); // stop window from scrolling
+      if (document.activeElement == maininput) {
+        first.focus();
+      } // if the currently focused element is the main input --> focus the first <li>
+      else if (document.activeElement == last) {
+        last.focus();
+      } // if we're at the bottom, stay there
+      else {
+        document.activeElement.parentElement.nextSibling.firstElementChild.focus();
+      } // otherwise select the next search result
+    }
+  }
+
+  // UP (38) arrow
+  if (event.keyCode == 38) {
+    if (searchVisible && resultsAvailable) {
+      event.preventDefault(); // stop window from scrolling
+      if (document.activeElement == maininput) {
+        maininput.focus();
+      } // If we're in the input box, do nothing
+      else if (document.activeElement == first) {
+        maininput.focus();
+      } // If we're at the first item, go to input box
+      else {
+        document.activeElement.parentElement.previousSibling.firstElementChild.focus();
+      } // Otherwise, select the search result above the current active one
+    }
+  }
+});
+
+function hideOnClickOutside(element) {
+  const outsideClickListener = (event) => {
+    if (!element.contains(event.target) && isVisible(element)) {
+      return true;
+      removeClickListener();
+    }
+  };
+
+  const removeClickListener = () => {
+    document.removeEventListener("click", outsideClickListener);
+  };
+
+  document.addEventListener("click", outsideClickListener);
+}
+
+const isVisible = (elem) =>
+  !!elem &&
+  !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length); // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+// ==========================================
+// execute search as each character is typed
+//
+document.getElementById("searchInput").onkeyup = function (e) {
+  executeSearch(this.value);
+};
+
+// ==========================================
+// fetch some json without jquery
+//
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open("GET", path);
+  httpRequest.send();
+}
+
+// ==========================================
+// load our search index, only executed once
+// on first call of search box (CMD-/)
+//
+function loadSearch() {
+  fetchJSONFile("/index.json", function (data) {
+    var options = {
+      // fuse.js options; check fuse.js website for details
+      shouldSort: true,
+      location: 0,
+      distance: 100,
+      threshold: 0.4,
+      minMatchCharLength: 2,
+      keys: ["title", "content"],
+    };
+    fuse = new Fuse(data, options); // build the index from the json file
+    // console.log(fuse);
+  });
+}
+
+// ==========================================
+// using the index we loaded on CMD-/, run
+// a search query (for "term") every time a letter is typed
+// in the search box
+//
+function executeSearch(term) {
+  let results = fuse.search(term); // the actual query being run using fuse.js
+  let searchitems = ""; // our results bucket
+
+  if (results.length === 0) {
+    // no results based on what was typed into the input box
+    resultsAvailable = false;
+    searchitems = "";
+  } else {
+    // build our html
+    // const reducer = (acc, curr) => {
+    //     console.log(curr);
+    //     acc + '<li><a href="' + curr.permalink + '" tabindex="0">' + '<span class="title">' + curr.title + '</span><br /> <span class="sc">'
+    //     + curr.section +'</span> — ' + curr.date + ' — <em>' + curr.desc + '</em></a></li>';
+    // }
+    var searchItems = results.slice(0, 5);
+    // console.log(JSON.stringify(searchItems));
+    for (let res in results.slice(0, 5)) {
+      // only show first 5 results
+
+      searchitems =
+        searchitems +
+        '<li><a href="' +
+        results[res].item.url +
+        '" tabindex="0" "style="width:100%">' +
+        '<span class="title">' +
+        results[res].item.title +
+        "</a></li>";
+    }
+    resultsAvailable = true;
+  }
+  //   console.log(searchitems)
+  document.getElementById("searchResults").innerHTML = searchitems;
+  if (results.length > 0) {
+    first = list.firstChild.firstElementChild; // first result container — used for checking against keyboard up/down location
+    last = list.lastChild.firstElementChild; // last result container — used for checking against keyboard up/down location
+  }
+}
