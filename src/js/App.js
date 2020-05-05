494c128d81e3446f909159f7412c17cfd4ae6792
Andy Gao <supagao7@gmail.com>
2020-05-05 11:33:39 -0400

    rebuilding site Tue May  5 11:33:39 EDT 2020

--- src/js/App.js
+++ src/js/App.js
@@ -14,6 +14,7 @@ import {
     faSearch
 } from '@fortawesome/free-solid-svg-icons';
 import {
+    faLink,
     faFacebook,
     faFacebookF,
     faGithub,
@@ -33,6 +34,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 
 library.add(
+    faLink,
     faSearch,
     faBookOpen,
     faChevronLeft,
